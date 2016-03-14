b6644d8cc1db9286e62c154bb8f894a0d36c2f0d
Liban Mohamed <liban.moh99@gmail.com>
2016-03-14 19:37:19 -0400

    Changes
    -Created a new CSS file for outlined button
    - Made changes to JS file so that if no quote tweet button does not open
    a new page

--- js/index.js
+++ js/index.js
@@ -1,12 +1,33 @@
+
+var quote= "";
+
 $(document).ready(function() {
 
+
+ 
   $("#getQuote").on("click", function() {
 
     $.getJSON("http://api.adviceslip.com/advice", function(json) {
 
-      $("#quote").html(JSON.stringify(json.slip.advice));
+		quote = json.slip.advice;    	
+
+    //  $("#quote").html(JSON.stringify(json.slip.advice));
+    $("#quote").html(quote)
 
     });
+
+  });
+
+  $('#tweet').on("click",function(){
+  	if(quote) {
+
+  			var tweet = "https://twitter.com/intent/tweet?text="+quote;
+  		$('#tweet').attr("href",tweet);
+
+  	}
+
+  		$("#tweet").attr("target","");
+
 
   });
 
